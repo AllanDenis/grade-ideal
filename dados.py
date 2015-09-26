286aa5b3c44567a3884f8ddfb4dc774bd53984c1
Allan Denis <allancomll@gmail.com>
2015-09-25 21:50:59 -0300

    Criadas funções de ranking e validação de grades.

--- dados.py
+++ dados.py
@@ -2,15 +2,22 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from scipy import linalg, sparse
+from scipy.sparse import *
+from scipy import linalg
 
 aulas_por_dia = 4
 dias_por_semana = 5
 qtd_disciplinas = 44
 
+# Para melhor legibilidade
+seg, ter, qua, qui, sex = range(dias_por_semana)
+aula1, aula2, aula3, aula4 = range(aulas_por_dia)
+
 # Matriz de dependências (MD), incluindo a disciplina nula (se o elemento a_ij == 1,
 # então a disciplina i depende da disciplina j,
 # caso contrário, a_ij == 0).
+
+#TODO: escolher o formato de matriz esparsa (http://docs.scipy.org/doc/scipy/reference/sparse.html#sparse-matrix-classes). 
 MD = np.array([
 	map(int, "011111011100001001011010000010011011111000111"),
 	map(int, "000000000000000000000000000000000000000000000"),
@@ -59,186 +66,62 @@ MD = np.array([
 	map(int, "000000000000000000000000000000000000000000000")
 ])
 
+# MD = csr_matrix(MD)
+
 # Grade semanal de aulas (4 aulas x 5 dias). 
 G = np.zeros((aulas_por_dia, dias_por_semana), dtype="int")
 
 # Matriz de aprovação (MA): lista de disciplinas em que o aluno foi aprovado
-MA = np.array(map(int, "111111111111111111011111110110000001000000011"))
+MA = csr_matrix(np.array(map(int, "111111111111111111011111110110000001000000011")))
+# MA = csr_matrix(np.array(map(int, "101101011110110001001000000000000000000000010")))
 
-# Para melhor legibilidade
-seg, ter, qua, qui, sex = range(dias_por_semana)
-aula1, aula2, aula3, aula4 = range(aulas_por_dia)
-
-
+disciplinas = []
+disc_inativas = set([0, 36, 41, 42])
+ 
 # Lista de disciplinas + 1 disciplina nula
-disciplinas = [
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#ORGE
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "11000"),
-		map(int, "11000")
-	]),
-	np.array([	#ALGO
-		map(int, "01010"),
-		map(int, "01010"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#INGL
-		map(int, "00100"),
-		map(int, "00100"),
-		map(int, "00001"),
-		map(int, "00001")
-	]),
-	np.array([	#CALC
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	np.array([	#NULA
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000"),
-		map(int, "00000")
-	]),
-	
-]
+for i in range(qtd_disciplinas + 1): disciplinas.append(csr_matrix(G.copy()))
+
+disciplinas[1] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula3,aula4), (seg,seg,ter,ter))), shape=(aulas_por_dia, dias_por_semana))	#ORGE
+disciplinas[2] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula1,aula2), (ter,ter,qui,qui))), shape=(aulas_por_dia, dias_por_semana))	#ALGO
+disciplinas[3] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula3,aula4), (qua,qua,sex,sex))), shape=(aulas_por_dia, dias_por_semana))	#INGL
+disciplinas[4] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula1,aula2), (qui,qui,sex,sex))), shape=(aulas_por_dia, dias_por_semana))	#CALC
+disciplinas[5] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula3,aula4), (seg,seg,qua,qua))), shape=(aulas_por_dia, dias_por_semana))	#PRSI
+disciplinas[6] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula1,aula2), (ter,ter,qui,qui))), shape=(aulas_por_dia, dias_por_semana))	#ESTD
+disciplinas[7] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula3,aula4), (qua,qua,qui,qui))), shape=(aulas_por_dia, dias_por_semana))	#ESTA
+disciplinas[8] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula3,aula4), (sex,sex,sex,sex))), shape=(aulas_por_dia, dias_por_semana))	#ARQC
+disciplinas[9] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula1,aula2), (seg,seg,qua,qua))), shape=(aulas_por_dia, dias_por_semana))	#SOPE
+disciplinas[10] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula3,aula4), (seg,seg,ter,ter))), shape=(aulas_por_dia, dias_por_semana))	#FNBD
+disciplinas[11] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula1,aula2), (ter,ter,qui,qui))), shape=(aulas_por_dia, dias_por_semana))	#PROO
+disciplinas[12] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula3,aula4), (ter,ter,sex,sex))), shape=(aulas_por_dia, dias_por_semana))	#RECO
+disciplinas[13] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula1,aula2), (seg,seg,sex,sex))), shape=(aulas_por_dia, dias_por_semana))	#APBD
+disciplinas[14] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula3,aula4), (qua,qua,qui,qui))), shape=(aulas_por_dia, dias_por_semana))	#IALI
+disciplinas[15] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula3,aula4), (seg,seg,qua,qua))), shape=(aulas_por_dia, dias_por_semana))	#FPIN
+disciplinas[16] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula1,aula2), (ter,ter,qua,qua))), shape=(aulas_por_dia, dias_por_semana))	#APRS
+disciplinas[17] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula1,aula2), (seg,seg,qui,qui))), shape=(aulas_por_dia, dias_por_semana))	#RHFI
+disciplinas[18] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula3,aula4), (seg,seg,sex,sex))), shape=(aulas_por_dia, dias_por_semana))	#PAVI
+disciplinas[19] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula3,aula4), (ter,ter,qua,qua))), shape=(aulas_por_dia, dias_por_semana))	#MEPC
+disciplinas[20] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula1,aula2), (qui,qui,sex,sex))), shape=(aulas_por_dia, dias_por_semana))	#LMMD
+disciplinas[21] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula3,aula4), (seg,seg,qui,qui))), shape=(aulas_por_dia, dias_por_semana))	#PJIN1
+disciplinas[22] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula1,aula2), (ter,ter,qui,qui))), shape=(aulas_por_dia, dias_por_semana))	#INHC
+disciplinas[23] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula1,aula2), (seg,seg,qua,qua))), shape=(aulas_por_dia, dias_por_semana))	#GEPJ
+disciplinas[24] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula3,aula4), (qua,qua,sex,sex))), shape=(aulas_por_dia, dias_por_semana))	#TABD
+disciplinas[25] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula1,aula2), (ter,ter,sex,sex))), shape=(aulas_por_dia, dias_por_semana))	#GESINF
+disciplinas[26] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula3,aula4), (ter,ter,qui,qui))), shape=(aulas_por_dia, dias_por_semana))	#GDSW
+disciplinas[27] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula3,aula4), (ter,ter,sex,sex))), shape=(aulas_por_dia, dias_por_semana))	#INCO
+disciplinas[28] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula1,aula2), (seg,seg,qui,qui))), shape=(aulas_por_dia, dias_por_semana))	#PRMK
+disciplinas[29] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula1,aula2), (qua,qua,sex,sex))), shape=(aulas_por_dia, dias_por_semana))	#SIGE
+disciplinas[30] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula3,aula4), (seg,seg,qua,qua))), shape=(aulas_por_dia, dias_por_semana))	#LSOR
+disciplinas[31] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula1,aula2), (ter,ter,qua,qua))), shape=(aulas_por_dia, dias_por_semana))	#FILO
+disciplinas[32] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula1,aula2), (seg,seg,qui,qui))), shape=(aulas_por_dia, dias_por_semana))	#PJIN2
+disciplinas[33] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula3,aula4), (seg,seg,sex,sex))), shape=(aulas_por_dia, dias_por_semana))	#SIAD
+disciplinas[34] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula3,aula4), (qua,qua,qua,qua))), shape=(aulas_por_dia, dias_por_semana))	#TESI1
+disciplinas[35] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula1,aula2), (qui,qui,sex,sex))), shape=(aulas_por_dia, dias_por_semana))	#INAR
+# disciplinas[36] = None	#COGR
+disciplinas[37] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula1,aula2), (ter,ter,sex,sex))), shape=(aulas_por_dia, dias_por_semana))	#SOCI
+disciplinas[38] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula3,aula4), (seg,seg,qua,qua))), shape=(aulas_por_dia, dias_por_semana))	#EMPR
+disciplinas[39] = csr_matrix(((1,1,1,1), ((aula3,aula4,aula3,aula4), (seg,seg,qui,qui))), shape=(aulas_por_dia, dias_por_semana))	#TESI2
+disciplinas[40] = csr_matrix(((1,1,1,1), ((aula1,aula2,aula3,aula4), (qui,qui,sex,sex))), shape=(aulas_por_dia, dias_por_semana))	#GOTI
+# disciplinas[41] = None	#TCC
+# disciplinas[42] = None	#ETIC
+disciplinas[43] = csr_matrix(((1,1		), ((aula1,aula2				), (ter,ter				))), shape=(aulas_por_dia, dias_por_semana))	#DIRL
+disciplinas[44] = csr_matrix(((1,1		), ((aula3,aula4				), (ter,ter				))), shape=(aulas_por_dia, dias_por_semana))	#LIBR
--- matricula.py
+++ matricula.py
@@ -5,45 +5,68 @@
 # ou juntar as folgas no mesmo dia
 
 from scipy import linalg, sparse
-from scipy.misc import comb
+from itertools import combinations
 import numpy as np
 import dados
 
 deps = dados.MD
 aprovadas = dados.MA
 
-deps = np.array([																	#teste
-		map(int, "010000"),
-		map(int, "001100"),
-		map(int, "000010"),	#teste
-		map(int, "000001"),
-		map(int, "000001"),
-		map(int, "000000")])	#teste							
-
-# aprovadas = np.array(map(int, "110100")) #teste
-aprovadas = np.array(map(int, raw_input("Digite seu vetor de aprovação (1" + ("x" * (len(deps)-1) )+ "): "))) #teste
- 
-# Soma dos requisitos de cada disciplina (soma das colunas)
 somaReq = deps.sum(axis=0)
 
 # Lista de disciplinas que o aluno pode cursar.
 cursaveis = aprovadas.dot(deps)
-cursaveis -= aprovadas
+cursaveis = cursaveis - aprovadas
 
 # Para evitar erro de div. por zero na disciplina nula e não permitir como cursável após divisão inteira
 somaReq[0] = -2 # Qualquer valor < -1
-cursaveis //= somaReq
+for i in range(cursaveis.shape[1] - 1):
+	cursaveis[0,i] //= somaReq[i]
 somaReq[0] = 0
+
+# Transforma em lista
+cursaveis = list(cursaveis.getA()[0])
 
 # Se o vetor de aprovação for inconsistente (disciplinas cumpridas sem todos os requisitos),
 # o vetor de cursáveis terá valores negativos.
 for i in cursaveis:
 	if i < 0: 
 		exit("Vetor de aprovação inválido. Verifique as dependências.")
+	
+print "Vetor de requisitos (SC):\t", "".join(map(str, somaReq))
+print "Vetor de aprovação (AP):\t", "".join(map(str, aprovadas.toarray()[0]))
+print "Vetor de cursáveis (DC):\t",		"".join(map(str, cursaveis))
+print "Número de grades possíveis:\t",	2 ** cursaveis.count(1) - 1
 
-print "Vetor de requisitos (SC):\t",			"".join(map(str, somaReq))
-print "Vetor de aprovação (AP):\t",			"".join(map(str, aprovadas))
-print "Vetor de cursáveis (DC):\t",			"".join(map(str, cursaveis))
-print "Número de grades possíveis:\t",	2 ** cursaveis.sum() - 1
-# print "# de grades possíveis:\t",		comb(cursaveis.sum(), 5, exact=True)
-print dados.disciplinas[0]
+# Obtém os IDs das disciplinas cursáveis
+cursaveis = set([x for x in xrange(len(cursaveis)) if cursaveis[x] == 1])
+cursaveis -= dados.disc_inativas
+
+# Retorna True se a grade não possuir conflitos
+def grade_valida(g):
+	if len(g.data) > 0:
+		return g.data.max() <= 1 # no máximo uma disciplina por aula
+
+# 0		= grade vazia
+# 100	= grade perfeita
+def grade_pontuacao(g):
+	pontos = 100 * (float(g.nnz) / (g.shape[0] * g.shape[1]))
+	dias_vazios = list(g.sum(axis=0).getA()[0])
+	# help(dias_vazios)
+	pontos *= 1 + (.05 * dias_vazios.count(0))
+	return pontos
+
+grades = []
+
+print cursaveis
+
+for i in range(7)[::-1]:
+	for grade in combinations(cursaveis, i):
+		tmp = dados.disciplinas[0].copy()
+		for disc in grade:
+			tmp = tmp + dados.disciplinas[disc]
+		if grade_valida(tmp):
+			horario = tmp.copy()
+			grades.append((tmp.copy(), sorted(grade), grade_pontuacao(tmp)))
+			# if tmp.nnz == 20: print tmp.todense(), sorted(grade), "[PERFEITA]\n"
+for i in grades: print i[0].todense(), i[1], i[2], "\n"
