4e452cf965c6ac489c28531fda6c27e925a02f72
Allan Denis <allancomll@gmail.com>
2015-11-10 22:54:46 -0300

    Resolvido problema de cross-domain (CORS instalado).

--- hello-flask.py
+++ hello-flask.py
@@ -1,39 +1,27 @@
 from flask import Flask, jsonify
+from flask.ext.cors import CORS
 import matricula
 import modelo
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/')
-def index():
-    return 'Index Page'
-
-@app.route('/hello')
-def hello():
-    return 'Hello World'
-
-@app.route('/disciplinas')
+@app.route('/disciplinas', methods = ['GET'])
 def lista_disciplinas():
     disciplinas = []
-    # for d in modelo.Disciplina.select():
-        # disciplinas.append(dict(d))
-    tabela = modelo.Disciplina.select()
-    for d in tabela:
+    for d in modelo.Disciplina.select():
         disc_tmp = {
             "id"        :   d.id,
             "nome"      :   d.nome,
             "sigla"     :   d.sigla,
             "periodo"   :   d.periodo,
             "ativa"     :   d.ativa,
-            # "cursos"    :   d.cursos,
         }
-        disciplinas.append(dict(disc_tmp))
-    # help(disciplinas[0])
-    # disciplinas = {1:"orge",2:"calc"}
+        disciplinas.append(disc_tmp)
     return jsonify(enumerate(disciplinas))
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
+@app.route('/grade', methods = ['POST'])
+def melhor_grade(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
 
