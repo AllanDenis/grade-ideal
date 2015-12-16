d4e53af95d6bd39d8e5b46cd6e530adc77415da3
Allan Denis <allancomll@gmail.com>
2015-12-16 17:47:40 -0300

    Reset feito para HEAD~2.

--- api_flask.py
+++ api_flask.py
@@ -1,11 +1,11 @@
 from flask import Flask, jsonify, request
-from flask.ext.cors import CORS
-from flask.ext.compress import Compress
-import matricula, modelo
 
-app = Flask(__name__)
-CORS(app)
-Compress(app)
+import matricula, modelo
+from banco import app
+
+#app = Flask(__name__)
+#CORS(app)
+#Compress(app)
 
 @app.route('/disciplinas', methods = ['GET'])
 def lista_disciplinas():
